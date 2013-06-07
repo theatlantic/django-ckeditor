4cbc3a8cd41507b27f8e4e2eda0770965bcf6fe7
Frankie Dintino <fdintino@theatlantic.com>
2013-06-07 11:11:13 -0400

    Updated ckeditor version to 3.6.3-atl-1.12

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='django-ckeditor',
-    version='3.6.3',
+    version=__import__('ckeditor').__version__,
     description='Django admin CKEditor integration.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Shaun Sephton',
