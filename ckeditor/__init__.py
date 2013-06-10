bd0b68eb7af140f5a57e78fcf3773d95966b1461
Frankie Dintino <fdintino@theatlantic.com>
2013-06-10 19:23:44 -0400

    Bump version to 3.6.3-atl-1.13

--- ckeditor/__init__.py
+++ ckeditor/__init__.py
@@ -1,4 +1,4 @@
 __version_info__ = (3, 6, 3)
-__atl_version_info__ = (1, 12)
+__atl_version_info__ = (1, 13)
 __version__ = '%s-atl-%s' % tuple(
     ['.'.join(map(str, v)) for v in [__version_info__, __atl_version_info__]])
