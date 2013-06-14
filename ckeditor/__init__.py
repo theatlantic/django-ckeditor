699101e03cfec55033fd63587794b2238abf1629
Frankie Dintino <fdintino@theatlantic.com>
2013-06-13 23:24:27 -0400

    Update ckeditor js to 4.1.2

--- ckeditor/__init__.py
+++ ckeditor/__init__.py
@@ -1,4 +1,2 @@
-__version_info__ = (4, 1, 1)
-__atl_version_info__ = (1, 00)
-__version__ = '%s-atl-%s' % tuple(
-    ['.'.join(map(str, v)) for v in [__version_info__, __atl_version_info__]])
+__version_info__ = (4, 1, 2)
+__version__ = '.'.join(map(str, __version_info__))
