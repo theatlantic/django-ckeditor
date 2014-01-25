e1f1dcd6156c6900d702dc78183fd72035e1629a
Frankie Dintino <fdintino@theatlantic.com>
2014-01-24 19:58:10 -0500

    Change django.conf.urls import for Django 1.5+. fixes #4

--- ckeditor/urls.py
+++ ckeditor/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    # Django <= 1.3
+    from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns(
-    '',
+
+urlpatterns = patterns('',
     url(r'^upload/', 'ckeditor.views.upload', name='ckeditor_upload'),
     url(r'^browse/', 'ckeditor.views.browse', name='ckeditor_browse'),
     url(r'^configs/', 'ckeditor.views.configs', name='ckeditor_configs'),
-    url(r'^fb_upload/', 'ckeditor.views.fb_upload', name='ckeditor_fb_upload'),
-)
+    url(r'^fb_upload/', 'ckeditor.views.fb_upload', name='ckeditor_fb_upload'))
