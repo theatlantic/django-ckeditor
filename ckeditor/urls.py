3d054e382aa5648a16414edbbaa7be17ee701d96
Frankie Dintino <fdintino@theatlantic.com>
2016-01-29 16:33:29 -0500

    Fix DeprecationWarning by changing django.conf.urls.patterns() to list

--- ckeditor/urls.py
+++ ckeditor/urls.py
@@ -1,12 +1,15 @@
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:
     # Django <= 1.3
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
+
+import ckeditor.views
 
 
-urlpatterns = patterns('',
-    url(r'^upload/', 'ckeditor.views.upload', name='ckeditor_upload'),
-    url(r'^browse/', 'ckeditor.views.browse', name='ckeditor_browse'),
-    url(r'^configs/', 'ckeditor.views.configs', name='ckeditor_configs'),
-    url(r'^fb_upload/', 'ckeditor.views.fb_upload', name='ckeditor_fb_upload'))
+urlpatterns = [
+    url(r'^upload/', ckeditor.views.upload, name='ckeditor_upload'),
+    url(r'^browse/', ckeditor.views.browse, name='ckeditor_browse'),
+    url(r'^configs/', ckeditor.views.configs, name='ckeditor_configs'),
+    url(r'^fb_upload/', ckeditor.views.fb_upload, name='ckeditor_fb_upload'),
+]
